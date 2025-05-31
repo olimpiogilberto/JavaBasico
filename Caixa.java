3356e427dd3d5369619fb1875e8e1df8adc15605
olimpiogilberto <olimpiogilberto@hotmail.com>
2025-05-30 22:45:48 -0300

    exclusao de lixo

--- Caixa.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-class Caixa
-{
-	public static void main(String[] args)
-	{
-		JOptionPane.showMessageDialog(null, "Bem vindo ao programa de cálculo de notas", 
-		"Programa de Cálculo", JOptionPane.ERROR_MESSAGE);
-	}//finish main
-}/*finish class*/
--- JOptionPane.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-class CaixaMessage 
-{	
-	public static void main (String args[])
-	{	
-		JOptionPane.showMessageDialog(null, "Bem vindo ao programa de cálculo de notas", 
-		"Programa de Cálculo", JOptionPane.INFORMATION_MESSAGE);
-	}	
-}
